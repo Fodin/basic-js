e541382d7baab995a9218253cd52943431b844de
Fedor Odintsov <fadmin@list.ru>
2021-02-13 13:56:10 +0300

    refactor: vigenere cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -8,40 +8,42 @@ class VigenereCipheringMachine {
         this.reverseType = type === false;
     }
 
-    encrypt (message, key) {
-        if (message === undefined || key === undefined) {
-            throw new Error()
-        }
+	encrypt (message, key) {
+		return this.encryptDecrypt(message, key, true)
+	}
+	
+	decrypt (message, key) {
+		return this.encryptDecrypt(message, key, false)
+	}
+
+    encryptDecrypt (message, key, encrypt) {
+        if (message === undefined || key === undefined) throw new Error()
 
         let shift = 0;
-        key = key.toUpperCase().split``.map(c => c.charCodeAt(0) - CODE_OF_A);
+		
+        key = key
+			.toUpperCase()
+			.split``
+			.map(c => c.charCodeAt(0) - CODE_OF_A);
+			
         message = message
             .toUpperCase()
             .split``
             .map((item, i) => (/[A-Z]/.test(item))
-                ? String.fromCharCode((item.charCodeAt(0) - CODE_OF_A + key[(i - shift) % key.length])
-                        % LETTERS_IN_ALPHABET + CODE_OF_A)
+                ? String.fromCharCode(
+					(
+						item.charCodeAt(0) 
+						- CODE_OF_A 
+						+ !encrypt * LETTERS_IN_ALPHABET 
+						+ (encrypt * 2 - 1) * key[(i - shift) % key.length]
+					)
+                    % LETTERS_IN_ALPHABET
+					+ CODE_OF_A)
                 : (shift++, item));
 
-        return this.reverseType ? message.reverse().join`` : message.join``;
-    }
-
-    decrypt (message, key) {
-        if (message === undefined || key === undefined) {
-            throw new Error()
-        }
-
-        let shift = 0;
-        key = key.toUpperCase().split``.map(c => c.charCodeAt(0) - CODE_OF_A);
-        message = message
-            .toUpperCase()
-            .split``
-            .map((item, i) => (/[A-Z]/.test(item))
-                ? String.fromCharCode((item.charCodeAt(0) - CODE_OF_A + LETTERS_IN_ALPHABET - key[(i - shift) % key.length])
-                        % LETTERS_IN_ALPHABET + CODE_OF_A)
-                : (shift++, item));
-
-        return this.reverseType ? message.reverse().join`` : message.join``;
+        return this.reverseType 
+			? message.reverse().join``
+			: message.join``;
     }
 }
 
